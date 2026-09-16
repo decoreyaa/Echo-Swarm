b83313076e8f1c2bd45dd919d95a2a85a407ee32
Decoreya Stewart <decoreya124@gmail.com>
2026-09-16 02:07:12 -0400

    Player looks and shoot in the direction of the mouse

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,23 +2,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Rigidbody2D rb;
     public float speed;
     public float cooldownTimer = 3f;
     public float timer = 0f;
+    private Vector3 mousePos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * speed;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
     // Update is called once per frame
     void Update()
-    {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+     {
+    //     transform.position += Vector3.right * speed * Time.deltaTime;
+    //     timer += Time.deltaTime; // count up in seconds
 
-        timer += Time.deltaTime; // count up in seconds
-
-        if (timer >= cooldownTimer)
-        {
-            Destroy(gameObject);
-            timer = 0f; // reset the timer
-        }
+    //     if (timer >= cooldownTimer)
+    //     {
+    //         Destroy(gameObject);
+    //     }
         
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Bullet bulletPrefab;
     private Rigidbody2D rb;
     private PlayerInput playerInput;
     private PlayerInput playerInputActions;
@@ -23,9 +24,37 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 movementInput = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        rb.AddForce(new Vector2(movementInput.x, movementInput.y) * moveSpeed, ForceMode2D.Force);
+        rb.linearVelocity = movementInput * moveSpeed;
+
+        
+    }
+    
+    void Update()
+    {
+        // Handle player input and movement here
+        if (playerInputActions.Player.Attack.triggered)
+        {
+            Shoot();
+        }
+
+        MousePosition();
     }
 
+   
 
+    private void Shoot()
+    {
+        // Implement shooting logic here
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Debug.Log("Shoot!");
+    }
+
+    private void MousePosition()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 
 }
